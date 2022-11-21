e831724dc5658abc1d2ac3398ac66292d37e2f21
Clijmart <mart.clijnk@student.hu.nl>
2022-11-21 14:02:47 +0100

    Added game basics

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+    [SerializeField] private Rigidbody body;
+
+    void Start()
+    {
+        body.velocity = new Vector3(speed, 0, 0);
+    }
+
+    private void Update()
+    {
+        Debug.Log($"Ball speed: {body.velocity.magnitude}");
+    }
+}
