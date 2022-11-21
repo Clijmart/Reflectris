9a9ad2ac8b99cccec5295dcf4761035b7ab0b158
Clijmart <mart.clijnk@live.nl>
2022-11-21 22:12:29 +0100

    Trying out custom physics

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,16 +4,57 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5;
-    [SerializeField] private Rigidbody body;
+    [SerializeField]
+    private float speed = 0.01f;
 
-    void Start()
+    private Vector3 movement;
+    private Collider[] colliders;
+
+    private void Start()
     {
-        body.velocity = new Vector3(speed, 0, 0);
+        movement = new Vector3(speed, 0, 0);
     }
 
     private void Update()
     {
-        Debug.Log($"Ball speed: {body.velocity.magnitude}");
+        transform.Translate(movement);
+
+        colliders = Physics.OverlapSphere(transform.position, 0.0f);
+        if (colliders.Length > 0)
+        {
+            Collider collider = colliders[0];
+            Debug.Log("Inside " + collider.name);
+
+            Vector3 normal = collider.transform.forward;
+            Debug.Log("normal: " + normal);
+            Vector3 reflected = Vector3.Reflect(movement, normal);
+            Debug.Log("reflected: " + reflected);
+            movement.x = reflected.x;
+            movement.z = reflected.z;
+
+            bool straightWall = Mathf.RoundToInt(collider.transform.rotation.eulerAngles.y) % 90 == 0;
+            if (!straightWall)
+            {
+                Debug.Log("Not straight wall! " + collider.transform.rotation.eulerAngles.y);
+                transform.position = collider.transform.position;
+            }
+
+            if (!collider.gameObject.CompareTag("Border"))
+            {
+                Destroy(collider.gameObject);
+            }
+        }
+
+        RoundPosition();
+    }
+
+    private void RoundPosition()
+    {
+        Vector3 rawPosition = transform.position;
+
+        rawPosition.x = Mathf.Round(rawPosition.x * 1000f) / 1000f;
+        rawPosition.z = Mathf.Round(rawPosition.z * 1000f) / 1000f;
+
+        transform.position = rawPosition;
     }
 }
--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -13,6 +13,5 @@ public class WallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
