b04b37ce69895d497b1c00dc6bb6cc1e29620653
Clijmart <mart.clijnk@student.hu.nl>
2023-01-16 16:00:33 +0100

    Add collision checks and block ghost

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,29 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject placeBlock;
+    private GameObject blockFrefab;
+
+    [SerializeField]
+    private GameObject ghostPrefab;
+
+    [SerializeField]
+    private LayerMask solidLayer;
+
+    private GameObject ghostBlock;
 
     void Update()
     {
+        PlaceGhost();
+
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Click detected");
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 200.0f))
-            {
-                if (!hit.transform.gameObject.CompareTag("Grid")) return;
-
-                Vector3 spawnPosition = new Vector3(0.5f, hit.point.y + 0.5f, 0.5f);
-                spawnPosition.x = Mathf.Round(hit.point.x - 0.5f) + spawnPosition.x;
-                spawnPosition.z = Mathf.Round(hit.point.z - 0.5f) + spawnPosition.z;
-                Instantiate(placeBlock, spawnPosition, Quaternion.identity);
-                Debug.Log("You clicked the " + hit.transform.name);
-            }
+            PlaceBlock();
         }
+    }
+
+    void PlaceBlock()
+    {
+        Debug.Log("Click detected");
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 200.0f))
+        {
+            if (!hit.transform.gameObject.CompareTag("Grid")) return;
+
+            Vector3 spawnPosition = SnapToGrid(hit.point);
+
+            // ToDo: Replace with collide check for each separate wall piece!
+            bool willCollide = Physics.CheckSphere(spawnPosition, 0.5f, solidLayer);
+            if (willCollide)
+            {
+                Debug.Log("Can't place!");
+                return;
+            }
+
+            Instantiate(blockFrefab, spawnPosition, Quaternion.identity);
+            Debug.Log("You clicked the " + hit.transform.name);
+        }
+    }
+
+    void PlaceGhost()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 200.0f))
+        {
+            if (!hit.transform.gameObject.CompareTag("Grid")) return;
+
+            Vector3 spawnPosition = SnapToGrid(hit.point);
+
+            // ToDo: Replace with collide check for each separate wall piece!
+            bool willCollide = Physics.CheckSphere(spawnPosition, 0.5f, solidLayer);
+            if (willCollide) return;
+
+            if (ghostBlock != null)
+            {
+                if (spawnPosition == ghostBlock.transform.position) return;
+
+                Destroy(ghostBlock);
+            }
+
+            ghostBlock = Instantiate(ghostPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    Vector3 SnapToGrid(Vector3 rawPosition)
+    {
+        Vector3 snapped = new Vector3(0.5f, rawPosition.y + 0.5f, 0.5f);
+        snapped.x = Mathf.Round(rawPosition.x - 0.5f) + snapped.x;
+        snapped.z = Mathf.Round(rawPosition.z - 0.5f) + snapped.z;
+
+        return snapped;
     }
 }
