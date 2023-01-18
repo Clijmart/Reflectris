e519cd6abc9e07fe2701d08619f7cabc9a52fbd8
Clijmart <mart.clijnk@live.nl>
2023-01-18 01:52:57 +0100

    Planned to add screen shaking

---
+++ Assets/Scripts/ScreenShakeController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeController : MonoBehaviour
+{
+    // ToDo using tutorial: https://www.youtube.com/watch?v=8PXPyyVu_6I
+}
