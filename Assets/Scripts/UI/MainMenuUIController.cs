9596d420580afbef09705af55d1f67b0f7e54c0f
Clijmart <mart.clijnk@live.nl>
2023-01-24 22:47:03 +0100

    Added keyboard shotcuts for main screen buttons

--- Assets/Scripts/UI/MainMenuUIController.cs
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -9,4 +9,20 @@ public class MainMenuUIController : MenuUI
     {
         GameObject.FindGameObjectWithTag("BackgroundAudio").GetComponent<BackgroundAudio>().PlayMusic();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Slash))
+        {
+            SceneManager.instance.StartGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            SceneManager.instance.OpenStats();
+        }
+        if (Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.Equals))
+        {
+            SceneManager.instance.OpenSettings();
+        }
+    }
 }
